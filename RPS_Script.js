17296b665bb340ba933bc4a536282885341960e2
Tollkien <AustinJBreaux@outlook.com>
2022-09-07 19:35:13 -0500

    Remove first letter capitalization of Rock/Paper/Scissors values for case insensitivity

--- RPS_Script.js
+++ RPS_Script.js
@@ -1,13 +1,13 @@
- /*Chooses a random number between 1-3*/
+ /*Chooses a random number between 1-3, and based on that logs rock/paper/scissors to the console*/
 let getComputerChoice = function randomChoice(){
     let currentRoundChoice = Math.floor(Math.random()*3)+1;
     if(currentRoundChoice === 1){
-        return console.log(Rock);
+        return console.log(rock);
     }
     else if(currentRoundChoice === 2){
-        return console.log(Paper);
+        return console.log(paper);
     }
     else if(currentRoundChoice === 3){
-        return console.log(Scissors);
+        return console.log(scissors);
     }
 }
