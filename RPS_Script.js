02ded6afbfc08596d63213b59f5fba58fe7657d0
Tollkien <AustinJBreaux@outlook.com>
2022-09-07 19:24:36 -0500

    Made the random number generator into a function that chooses between 1-3, then logs rock/paper/scissors to the console based on that number choice

--- RPS_Script.js
+++ RPS_Script.js
@@ -1,1 +1,13 @@
-console.log(Math.floor(Math.random()*3))
+ /*Chooses a random number between 1-3*/
+let getComputerChoice = function randomChoice(){
+    let currentRoundChoice = Math.floor(Math.random()*3)+1;
+    if(currentRoundChoice === 1){
+        return console.log(Rock);
+    }
+    else if(currentRoundChoice === 2){
+        return console.log(Paper);
+    }
+    else if(currentRoundChoice === 3){
+        return console.log(Scissors);
+    }
+}
