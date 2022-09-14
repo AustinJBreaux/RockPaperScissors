29d356ecef0b75f6e4bd70b97731c8af99434d87
Tollkien <AustinJBreaux@outlook.com>
2022-09-13 19:56:36 -0500

    getPlayerChoice now also tests against the computerChoice and evaluates the result of the game

--- RPS_Script.js
+++ RPS_Script.js
@@ -2,13 +2,13 @@
 let getComputerChoice = function randomChoice(){
     let currentRoundChoice = Math.floor(Math.random()*3)+1;
     if(currentRoundChoice === 1){
-        return console.log(rock);
+        return currentRoundChoice = 1;
     }
     else if(currentRoundChoice === 2){
-        return console.log(paper);
+        return currentRoundChoice = 2;
     }
     else if(currentRoundChoice === 3){
-        return console.log(scissors);
+        return currentRoundChoice = 3;
     }
 }
 let getPlayerChoice = function playerChoice(){
@@ -19,7 +19,51 @@ let getPlayerChoice = function playerChoice(){
     let playerResponse = playerAnswer.toLowerCase();
     if(playerResponse === op1 || playerResponse === op2
     || playerResponse === op3){
-        console.log(playerResponse);
+        getComputerChoice();
+        switch(getComputerChoice()){
+            case 1: /*Comp rock*/
+                    if(playerResponse === op1){
+                        return "It's a tie!"
+                    }
+                    else if(playerResponse === op2){
+                        return "Player wins!"
+                    }
+                    else if(playerResponse === op3){
+                        return "Computer wins!"
+                    }
+                    else{
+                        console.log("Error")
+                    }
+                break;
+            case 2:/*Comp paper */
+                    if(playerResponse === op1){
+                        return "Computer wins!!"
+                    }
+                    else if(playerResponse === op2){
+                        return "It's a tie!"
+                    }
+                    else if(playerResponse === op3){
+                        return "Player Wins!"
+                    }
+                    else{
+                        console.log("Error")
+                    }
+                break;
+            case 3:/*Comp scissors*/
+                    if(playerResponse === op1){
+                        return "Player wins!"
+                    }
+                    else if(playerResponse === op2){
+                        return "Computer wins!"
+                    }
+                    else if(playerResponse === op3){
+                        return "It's a tie!"
+                    }
+                    else{
+                        console.log("Error")
+                    }
+                break;
+        }
     }
     else{
         console.log("Please check your spelling and try again");
