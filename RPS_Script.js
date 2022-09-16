c04a087f3e0a1756f39ffae4b3c0b3c34717de35
Tollkien <AustinJBreaux@outlook.com>
2022-09-15 21:05:17 -0500

    Changed some debugging code that was causing errors, the game will now play. UNFINISHED game cannot end

--- RPS_Script.js
+++ RPS_Script.js
@@ -1,4 +1,8 @@
- /*Chooses a random number between 1-3, and based on that logs rock/paper/scissors to the console*/
+/*Declares global variables*/
+let playerWinCounter = 0;
+let computerWinCounter = 0;
+
+/*Chooses a random number between 1-3, and based on that logs rock/paper/scissors to the console*/
 let getComputerChoice = function randomChoice(){
     let currentRoundChoice = Math.floor(Math.random()*3)+1;
     if(currentRoundChoice === 1){
@@ -11,6 +15,8 @@ let getComputerChoice = function randomChoice(){
         return currentRoundChoice = 3;
     }
 }
+
+/*Get player's input, and test it against the random computer choice*/
 let getPlayerChoice = function playerChoice(){
     let playerAnswer = prompt("Please choose between rock, paper, or scissors","Rock, Paper, Scissors");
     let op1 = "rock";
@@ -21,59 +27,41 @@ let getPlayerChoice = function playerChoice(){
     || playerResponse === op3){
         switch(getComputerChoice()){
             case 1: /*Comp rock*/
-                    if(playerResponse === op1){
+                    if(playerResponse === op1){/*Player rock*/
                         return "It's a tie!"
                     }
-                    else if(playerResponse === op2){
-                        if(playerWinCounter != undefined){
-                            playerWinCounter +=1;
+                    else if(playerResponse === op2){/*Player paper*/
+                        playerWinCounter +=1;
                         }
-                        return "Player wins!";
-                    }
-                    else if(playerResponse === op3){
-                        if(playerWinCounter != undefined){
-                            computerWinCounter +=1;
-                        }
-                        return "Computer wins!";
+                    else if(playerResponse === op3){/*Player scissors*/
+                        computerWinCounter +=1;
                     }
                     else{
                         console.log("Error")
                     }
                 break;
             case 2:/*Comp paper */
-                    if(playerResponse === op1){
-                        if(playerWinCounter != undefined){
-                            computerWinCounter +=1;
-                        }
-                        return "Computer wins!";
+                    if(playerResponse === op1){ /*Player rock*/
+                        computerWinCounter +=1;
                     }
-                    else if(playerResponse === op2){
+                    else if(playerResponse === op2){/*Player paper*/
                         return "It's a tie!"
                     }
-                    else if(playerResponse === op3){
-                        if(playerWinCounter != undefined){
+                    else if(playerResponse === op3){/*Player scissors*/
                             playerWinCounter +=1;
                         }
-                        return "Player Wins!";
-                    }
                     else{
                         console.log("Error")
                     }
                 break;
             case 3:/*Comp scissors*/
                     if(playerResponse === op1){
-                        if(playerWinCounter != undefined){
-                            playerWinCounter +=1;
+                        playerWinCounter +=1;
                         }
-                        return "Player wins!";
+                    else if(playerResponse === op2){/*Player paper*/
+                        computerWinCounter +=1;
                     }
-                    else if(playerResponse === op2){
-                        if(playerWinCounter != undefined){
-                            computerWinCounter +=1;
-                        }
-                        return "Computer wins!";
-                    }
-                    else if(playerResponse === op3){
+                    else if(playerResponse === op3){/*Player scissors*/
                         return "It's a tie!"
                     }
                     else{
@@ -87,12 +75,12 @@ let getPlayerChoice = function playerChoice(){
         getPlayerChoice();
     }
 }
+
+/*Function to start the game */
 playGame = function gameStart(){
-    let playerWinCounter = 0;
-    let computerWinCounter = 0;
     do{
-        getPlayerChoice(playerWinCounter, computerWinCounter);
         console.log(`Player:${playerWinCounter}. Computer:${computerWinCounter}`)
+        getPlayerChoice();
     }
     while(playerWinCounter < 5 || computerWinCounter < 5){
         if(playerWinCounter === 5){
