c7686563759fc978d669dd72e132cbfd6fd2832e
Tollkien <AustinJBreaux@outlook.com>
2022-10-20 21:11:43 -0500

    Makes the code to choose the  winner into a function, and refactors code

--- RPS_Script.js
+++ RPS_Script.js
@@ -8,6 +8,19 @@ let computerWinCounter = 0;
 document.addEventListener('click', function(e){
     console.log(e.class);
 });
+
+/*
+TO DO:
+Set an if function for each of the 3 buttons, if button.rock set __ = rock
+Then copy/paste that switch down there to it's own function, run the ___
+as an argument in that, and the rest of the code should work from there
+*/
+/*Get player's input, and test it against the random computer choice*/
+
+let getPlayerChoice = function playerChoice(e){
+    if(e = button){}
+}
+
 /*Chooses a random number between 1-3, and based on that logs rock/paper/scissors to the console*/
 let getComputerChoice = function randomChoice(){
     let currentRoundChoice = Math.floor(Math.random()*3)+1;
@@ -21,62 +34,52 @@ let getComputerChoice = function randomChoice(){
         return currentRoundChoice = 3;
     }
 }
-/*
-TO DO:
-Set an if function for each of the 3 buttons, if button.rock set __ = rock
-Then copy/paste that switch down there to it's own function, run the ___
-as an argument in that, and the rest of the code should work from there
-*/
-/*Get player's input, and test it against the random computer choice*/
-let getPlayerChoice = function playerChoice(e){
-    if(e = button){
-        switch(getComputerChoice()){
-            case 1: /*Comp rock*/
-                    if(playerResponse === "rock"){/*Player rock*/
-                        return console.log("It's a tie!")
+
+/*Chooses winner*/
+const chooseWinner = function chooseWinner(){
+    switch(getComputerChoice()){
+        case 1: /*Comp rock*/
+                if(playerResponse === "rock"){/*Player rock*/
+                    return console.log("It's a tie!")
+                }
+                else if(playerResponse === "paper"){/*Player paper*/
+                    playerWinCounter +=1;
                     }
-                    else if(playerResponse === "paper"){/*Player paper*/
+                else if(playerResponse === "scissors"){/*Player scissors*/
+                    computerWinCounter +=1;
+                }
+                else{
+                    console.log("Error")
+                }
+            break;
+        case 2:/*Comp paper */
+                if(playerResponse === "rock"){ /*Player rock*/
+                    computerWinCounter +=1;
+                }
+                else if(playerResponse === "paper"){/*Player paper*/
+                    return console.log("It's a tie!")
+                }
+                else if(playerResponse === "scissors"){/*Player scissors*/
                         playerWinCounter +=1;
-                        }
-                    else if(playerResponse === "scissors"){/*Player scissors*/
-                        computerWinCounter +=1;
                     }
-                    else{
-                        console.log("Error")
+                else{
+                    console.log("Error")
+                }
+            break;
+        case 3:/*Comp scissors*/
+                if(playerResponse === "rock"){
+                    playerWinCounter +=1;
                     }
-                break;
-            case 2:/*Comp paper */
-                    if(playerResponse === "rock"){ /*Player rock*/
-                        computerWinCounter +=1;
-                    }
-                    else if(playerResponse === "paper"){/*Player paper*/
-                        return console.log("It's a tie!")
-                    }
-                    else if(playerResponse === "scissors"){/*Player scissors*/
-                            playerWinCounter +=1;
-                        }
-                    else{
-                        console.log("Error")
-                    }
-                break;
-            case 3:/*Comp scissors*/
-                    if(playerResponse === "rock"){
-                        playerWinCounter +=1;
-                        }
-                    else if(playerResponse === "paper"){/*Player paper*/
-                        computerWinCounter +=1;
-                    }
-                    else if(playerResponse === "scissors"){/*Player scissors*/
-                        return console.log("It's a tie!")
-                    }
-                    else{
-                        console.log("Error")
-                    }
-                break;
-        }
-    }
-    else{
-        return;
+                else if(playerResponse === "paper"){/*Player paper*/
+                    computerWinCounter +=1;
+                }
+                else if(playerResponse === "scissors"){/*Player scissors*/
+                    return console.log("It's a tie!")
+                }
+                else{
+                    console.log("Error")
+                }
+            break;
     }
 }
 
