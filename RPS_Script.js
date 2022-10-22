2ecb6b3bbbd7a09d5f093d691b1ef4eee278f83a
Tollkien <AustinJBreaux@outlook.com>
2022-10-22 14:13:59 -0500

    Prints to console as intended. Finished

--- RPS_Script.js
+++ RPS_Script.js
@@ -10,11 +10,6 @@ document.addEventListener('click', function(e){
     if(e.target.tagName != 'BUTTON'){
         return;
     }
-    else if(e.target.tagName === 'Restart'){
-        let playerWinCounter = 0;
-        let computerWinCounter = 0;
-        console.log('Game has been reset!');
-    }
     else{
         let playerResponse = e.target.className;
         playGame(playerResponse);
@@ -28,17 +23,7 @@ document.addEventListener('click', function(e){
 /*Function to start the game */
 playGame = function gameStart(playerResponse){
     getPlayerChoice(playerResponse);
-    if(playerWinCounter <= 5){
-        return `Congrats! You win! Final score is ${playerWinCounter} to ${computerWinCounter}. Play again?`;
-    }
-    else if(computerWinCounter <= 5){
-        return `Computer wins! Better luck next time! Final score is ${computerWinCounter} to ${playerWinCounter}. Play again?`
-    }
-    else{
-        console.log(`Current score - Player:${playerWinCounter}. Computer:${computerWinCounter}`);
-    }
 }
-
 /*Gets player choice based on button click*/
 let getPlayerChoice = function playerChoice(){
     if(playerResponse = 'rock'){
@@ -57,46 +42,76 @@ let getPlayerChoice = function playerChoice(){
 const chooseWinner = function chooseWinner(){
     switch(getComputerChoice()){
         case 1: /*Comp rock*/
-                if(playerResponse === "rock"){/*Player rock*/
-                    return console.log("It's a tie!")
+            if(playerResponse === "rock"){/*Player rock*/
+                return console.log("It's a tie!")
+            }
+            else if(playerResponse === "paper"){/*Player paper*/
+                playerWinCounter +=1;
+                if (playerWinCounter == 5){
+                    console.log(`Congrats! You win! Final score is ${playerWinCounter} to ${computerWinCounter}. 
+                    Refresh this window to play again!`);
                 }
-                else if(playerResponse === "paper"){/*Player paper*/
-                    playerWinCounter +=1;
-                    }
-                else if(playerResponse === "scissors"){/*Player scissors*/
-                    computerWinCounter +=1;
+                else console.log(`Current score - Player:${playerWinCounter}. Computer:${computerWinCounter}`);
                 }
-                else{
-                    console.log("Error")
+            else if(playerResponse === "scissors"){/*Player scissors*/
+                computerWinCounter +=1;
+                if(computerWinCounter == 5){
+                    console.log(`Computer wins! Better luck next time! Final score is ${computerWinCounter} to ${playerWinCounter}. 
+                    Refresh this window to play again!`);
                 }
+                else console.log(`Current score - Player:${playerWinCounter}. Computer:${computerWinCounter}`);
+            }
+            else{
+                console.log("Error")
+            }
             break;
         case 2:/*Comp paper */
-                if(playerResponse === "rock"){ /*Player rock*/
-                    computerWinCounter +=1;
+            if(playerResponse === "rock"){ /*Player rock*/
+                computerWinCounter +=1;
+                if(computerWinCounter == 5){
+                    console.log(`Computer wins! Better luck next time! Final score is ${computerWinCounter} to ${playerWinCounter}. 
+                    Refresh this window to play again!`);
                 }
-                else if(playerResponse === "paper"){/*Player paper*/
-                    return console.log("It's a tie!")
-                }
-                else if(playerResponse === "scissors"){/*Player scissors*/
-                        playerWinCounter +=1;
+                else console.log(`Current score - Player:${playerWinCounter}. Computer:${computerWinCounter}`);
+            }
+            else if(playerResponse === "paper"){/*Player paper*/
+                return console.log("It's a tie!")
+            }
+            else if(playerResponse === "scissors"){/*Player scissors*/
+                    playerWinCounter +=1;
+                    if (playerWinCounter == 5){
+                        console.log(`Congrats! You win! Final score is ${playerWinCounter} to ${computerWinCounter}. 
+                        Refresh this window to play again!`);
                     }
-                else{
-                    console.log("Error")
+                    else console.log(`Current score - Player:${playerWinCounter}. Computer:${computerWinCounter}`);
                 }
+            else{
+                console.log("Error")
+            }
             break;
         case 3:/*Comp scissors*/
-                if(playerResponse === "rock"){
-                    playerWinCounter +=1;
-                    }
-                else if(playerResponse === "paper"){/*Player paper*/
-                    computerWinCounter +=1;
+            if(playerResponse === "rock"){
+                playerWinCounter +=1;
+                if (playerWinCounter == 5){
+                    console.log(`Congrats! You win! Final score is ${playerWinCounter} to ${computerWinCounter}. 
+                    Refresh this window to play again!`);
                 }
-                else if(playerResponse === "scissors"){/*Player scissors*/
-                    return console.log("It's a tie!")
+                else console.log(`Current score - Player:${playerWinCounter}. Computer:${computerWinCounter}`);
                 }
-                else{
-                    console.log("Error")
+            else if(playerResponse === "paper"){/*Player paper*/
+                computerWinCounter +=1;
+                if(computerWinCounter == 5){
+                    console.log(`Computer wins! Better luck next time! Final score is ${computerWinCounter} to ${playerWinCounter}. 
+                    Refresh this window to play again!`);
                 }
+                else console.log(`Current score - Player:${playerWinCounter}. Computer:${computerWinCounter}`);
+            }
+            else if(playerResponse === "scissors"){/*Player scissors*/
+                return console.log("It's a tie!")
+            }
+            else{
+                console.log("Error")
+            }
             break;
     }
 }
